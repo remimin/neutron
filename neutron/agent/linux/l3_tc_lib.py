4fa7dcd84101abefe079e0fb7199f8e8095b4f2d
lic119 <lic119@chinaunicom.cn>
2020-04-15 17:32:21 +0800

    tc deals with the compatibility of ipv4 and ipv6

--- neutron/agent/linux/l3_tc_lib.py
+++ neutron/agent/linux/l3_tc_lib.py
@@ -25,10 +25,15 @@ LOG = logging.getLogger(__name__)
 
 QDISC_IN_REGEX = re.compile(r"qdisc ingress (\w+:) *")
 QDISC_OUT_REGEX = re.compile(r"qdisc htb (\w+:) *")
-FILTER_ID_REGEX = re.compile(r"filter protocol (?:ip|ipv6) u32 (fh|chain \d+ fh) (\w+::\w+) *")
+FILTER_ID_REGEX = re.compile(r"filter protocol (.*?) u32 (fh|chain \d+ fh) (\w+::\w+) *")
 FILTER_STATS_REGEX = re.compile(r"Sent (\w+) bytes (\w+) pkts *")
-IPV6_32BIT_ADDR_REGEX = re.compile(r"\s+match ([a-f0-9]*)/ffffffff at (\d+) ([(].*?[)])*")
+IPV4_ADDR_REGEX = re.compile(r"match ([a-f0-9]*)/ffffffff at (1[2,6]) *")
+IPV6_32BIT_ADDR_REGEX = re.compile(r"match ([a-f0-9]*)/ffffffff at (\d+) ([(].*?[)])*")
 IPV6_FULL_ADDR_REGEX = re.compile(r"match .*? ([a-f0-9:]*)/128 *")
+
+IPV4_START_OF_HEADER_OFFSET = ('12', '16')
+IPV6_START_OF_HEADER_OFFSET = ('8', '24')
+IPV6_END_OF_HEADER_OFFSET = ('20', '36')
 
 
 class FloatingIPTcCommandBase(ip_lib.IPDevice):
@@ -66,70 +71,91 @@ class FloatingIPTcCommandBase(ip_lib.IPDevice):
                'parent', qdisc_id, 'prio', 1]
         return self._execute_tc_cmd(cmd)
 
-    def _get_filters_v6(self, qdisc_id, ip):
-        ipv6_addr_offset_map = {}
-        ipv6_addr_list = []
-        ipv6_addr = ''
-        _match = 'dst'
-        success_packet_num_str = ''
+    def _get_filters_ips_map_v4(self, qdisc_id, ip):
+        filter_ip_map = dict()
+        filter_id = ''
+        is_first_match_line = bool()
         cmd = ['-s', '-d', 'filter', 'show', 'dev', self.name,
                'parent', qdisc_id, 'prio', 1]
         filters_output = self._execute_tc_cmd(cmd)
         if not filters_output:
             raise exceptions.FilterIDForIPNotFound(ip=ip)
         filter_lines = filters_output.split('\n')
-        # Traverse the copied list and manipulate the original list \
-        # in order to avoid deleting other elements by mistake
-        for line in filter_lines[:]:
-            # line = line.strip()
-            m = IPV6_32BIT_ADDR_REGEX.match(line)
-            if m:
-                part_of_ipv6_addr = m.group(1)
-                ipv6_header_offset = m.group(2)
-                ipv6_addr_offset_map[int(ipv6_header_offset)] = part_of_ipv6_addr
-                success_packet_num_str = m.group(3)
-                filter_lines.remove(line)
-        # src ipv6 address starts from the 8th byte of the IP header
-        # and dst ipv6 address starts from the 24th byte
+        for line in filter_lines:
+            line = line.strip()
+            m_filter_id = FILTER_ID_REGEX.match(line)
+            m_ipv4_addr = IPV4_ADDR_REGEX.match(line)
+            if m_filter_id and m_filter_id.group(1) == 'all':
+                filter_id = m_filter_id.group(3)
+                is_first_match_line = True
+                continue
+            if m_ipv4_addr and len(filter_id) != 0 and is_first_match_line:
+                ipv4_addr = '0x' + m_ipv4_addr.group(1)
+                ipv4_header_offset = m_ipv4_addr.group(2)
+                if ipv4_header_offset in IPV4_START_OF_HEADER_OFFSET:
+                    filter_ip_map[filter_id] = \
+                        str(netaddr.IPAddress(ipv4_addr))
+            is_first_match_line = False
+        return filter_ip_map
+
+    @staticmethod
+    def _format_ipv6_address(ipv6_addr_offset_map):
+        ipv6_addr = ''
+        ipv6_addr_list = list()
         for offset in sorted(ipv6_addr_offset_map):
             ipv6_addr_list.append(ipv6_addr_offset_map[offset][0:4])
             ipv6_addr_list.append(ipv6_addr_offset_map[offset][4:8])
-            if offset == 8:
-                _match = 'src'
+
         for ipv6_str in ipv6_addr_list:
             ipv6_addr += ipv6_str + ':'
-        ipv6_addr = ipv6_addr[:-1] + '/128'
-        filter_match_line = ['match', 'IPv6', _match, ipv6_addr, success_packet_num_str]
-        filter_match_line = ' '.join(filter_match_line)
-        filter_lines.append(filter_match_line)
-        return filter_lines
+        ipv6_addr = ipv6_addr[:-1]
+        return ipv6_addr
 
-    def _get_filterid_for_ip(self, qdisc_id, ip):
-        filter_id = ""
-        filterids_for_ip = []
-        if netaddr.IPAddress(ip).version == constants.IP_VERSION_4:
-            filters_output = self._get_filters(qdisc_id)
-            if not filters_output:
-                raise exceptions.FilterIDForIPNotFound(ip=ip)
-            filter_lines = filters_output.split('\n')
-        else:
-            filter_lines = self._get_filters_v6(qdisc_id, ip)
+    def _get_filters_ips_map_v6(self, qdisc_id, ip):
+        ipv6_addr_offset_map = dict()
+        filter_ip_map = dict()
+        filter_id = ''
+        cmd = ['-s', '-d', 'filter', 'show', 'dev', self.name,
+               'parent', qdisc_id, 'prio', 1]
+        filters_output = self._execute_tc_cmd(cmd)
+        if not filters_output:
+            raise exceptions.FilterIDForIPNotFound(ip=ip)
+        filter_lines = filters_output.split('\n')
         for line in filter_lines:
             line = line.strip()
-            m = FILTER_ID_REGEX.match(line)
-            if m:
-                filter_id = m.group(2)
-                # It matched, so ip/32 is not here. continue
+            m_filter_id = FILTER_ID_REGEX.match(line)
+            m_ipv6_addr = IPV6_32BIT_ADDR_REGEX.match(line)
+            if m_filter_id and m_filter_id.group(1) == 'all':
+                filter_id = m_filter_id.group(3)
                 continue
-            elif not line.startswith('match'):
-                continue
-            parts = line.split(" ")
-            if ip + '/32' in parts:
-                filterids_for_ip.append(filter_id)
-            elif IPV6_FULL_ADDR_REGEX.match(line):
+            if m_ipv6_addr and len(filter_id) != 0:
+                part_of_ipv6_addr = m_ipv6_addr.group(1)
+                ipv6_header_offset = m_ipv6_addr.group(2)
+                if ipv6_header_offset in IPV6_START_OF_HEADER_OFFSET:
+                    ipv6_addr_offset_map.clear()
+                ipv6_addr_offset_map[int(ipv6_header_offset)] \
+                    = part_of_ipv6_addr
+                if ipv6_header_offset in IPV6_END_OF_HEADER_OFFSET:
+                    ipv6_addr = \
+                        self._format_ipv6_address(ipv6_addr_offset_map)
+                    filter_ip_map[filter_id] = ipv6_addr
+                    filter_id = ''
+                    ipv6_addr_offset_map.clear()
+        return filter_ip_map
+
+    def _get_filterid_for_ip(self, qdisc_id, ip):
+        filterids_for_ip = list()
+        if netaddr.IPAddress(ip).version == constants.IP_VERSION_4:
+            filters_ips_map = self._get_filters_ips_map_v4(qdisc_id, ip)
+            for key, value in filters_ips_map.items():
+                if ip == value:
+                    filterids_for_ip.append(key)
+        else:
+            filters_ips_map = self._get_filters_ips_map_v6(qdisc_id, ip)
+            for key, value in filters_ips_map.items():
                 if socket.inet_pton(socket.AF_INET6, ip) == \
-                        socket.inet_pton(socket.AF_INET6, IPV6_FULL_ADDR_REGEX.match(line).group(1)):
-                    filterids_for_ip.append(filter_id)
+                        socket.inet_pton(socket.AF_INET6, value):
+                    filterids_for_ip.append(key)
         if len(filterids_for_ip) > 1:
             raise exceptions.MultipleFilterIDForIPFound(ip=ip)
         elif len(filterids_for_ip) == 0:
@@ -162,7 +188,7 @@ class FloatingIPTcCommandBase(ip_lib.IPDevice):
             tc_lib.TcCommand.get_ingress_qdisc_burst_value(rate, burst),
             tc_lib.BURST_UNIT
         )
-        protocol = ['protocol', 'ip']
+        protocol = ['protocol', 'all']
         prio = ['prio', 1]
         _match = 'src' if direction == constants.EGRESS_DIRECTION else 'dst'
         match = ['u32', 'match', 'ip', _match, ip]
@@ -179,7 +205,7 @@ class FloatingIPTcCommandBase(ip_lib.IPDevice):
             tc_lib.TcCommand.get_ingress_qdisc_burst_value(rate, burst),
             tc_lib.BURST_UNIT
         )
-        protocol = ['protocol', 'ipv6']
+        protocol = ['protocol', 'all']
         prio = ['prio', 1]
         _match = 'src' if direction == constants.EGRESS_DIRECTION else 'dst'
         match = ['u32', 'match', 'ip6', _match, ip]
